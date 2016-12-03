1fd72f6515967dab7d1c5fef5408327674941e26
Nick Ladd <laddng@gmail.com>
2016-12-03 12:16:41 -0500

    new logging module added

--- classes/NeuralNetwork.py
+++ classes/NeuralNetwork.py
@@ -4,6 +4,11 @@ from pybrain.tools.shortcuts import buildNetwork;
 from pybrain.supervised.trainers.backprop import BackpropTrainer;
 from matplotlib import pyplot as plt;
 import timeit;
+import logging;
+from logging.config import fileConfig;
+
+fileConfig('logging_config.ini');
+logger = logging.getLogger();
 
 class NeuralNetwork:
 	""" Neural network class """
@@ -15,10 +20,10 @@ class NeuralNetwork:
 		self.network = None;
 		self.backprop = None;
 		self.epochs = 10;
-		print("[NeuralNetwork]: New neural network object created.");
+		logger.info("New neural network object created.");
 
 	def run(self):
-		print("[NeuralNetwork]: Starting up neural network");
+		logger.info("Starting up neural network");
 		self.buildDataset();
 		self.buildNetwork();
 		self.addData([0,0], [0]);
@@ -34,33 +39,33 @@ class NeuralNetwork:
 
 	def buildDataset(self):
 		self.dataset = SupervisedDataSet(self.num_inputs, self.num_outputs);
-		print("[buildDataset]: Dataset object built.");
+		logger.info("Dataset object built.");
 		return True;
 
 	def addData(self, data_in, data_out):
 		self.dataset.addSample(data_in, data_out);
-		print("[addData]: Data appended.");
+		logger.info("Data appended.");
 		return True;
 
 	def buildNetwork(self):
 		self.network = buildNetwork(self.num_inputs, self.num_hidden_layers, self.num_outputs);
-		print("[buildNetwork]: Network created.");
+		logger.info("Network created.");
 		return True;
 
 	def backProp(self):
 		self.backprop = BackpropTrainer(self.network, learningrate = 0.01, momentum = 0.99);
-		print("[backProp]: Back propogation trainer created.");
+		logger.info("Back propogation trainer created.");
 		return True;
 
 	def testData(self):
-		print('[testData]: MSE:', self.backprop.testOnData(self.dataset));
+		logger.info('MSE: %s', str(self.backprop.testOnData(self.dataset)));
 		return True;
 
 	def trainData(self):
 		start = timeit.default_timer();
 		self.backprop.trainOnDataset(self.dataset, self.epochs);
 		stop = timeit.default_timer();
-		print("[trainData]: Training time: "+str(stop - start));
+		logger.info("Training time: %s", str(stop - start));
 		return True;
 
 	def graphResults(self):
